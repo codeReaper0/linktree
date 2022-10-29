aebe08f602bb3c51eee5cda435cee6f9ee99c176
Tella Boluwatife <boluwatifetella@gmail.com>
2022-10-29 17:10:27 +0100

    Added font Nunito

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1 className="font-bold">Whatsup gee</h1>);
+root.render(
+  <main>
+    <section>
+      <img
+        src="../public/avatar.jpg"
+        alt="Tella Boluwatife"
+        className="w-[88px] h-[88px] rounded-full align-middle"
+      ></img>
+    </section>
+  </main>
+);
 
 reportWebVitals();
